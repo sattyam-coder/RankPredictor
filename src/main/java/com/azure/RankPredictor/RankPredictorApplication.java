448c22646e8808c82d82b5f5177ca9e135df3727
Satyam Chaturvedi <satyam_chaturvedi@apple.com>
2023-03-30 09:24:03 +0530

    Initial commit.

---
+++ src/main/java/com/azure/RankPredictor/RankPredictorApplication.java
@@ -0,0 +1,22 @@
+package com.azure.RankPredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RankPredictorApplication {
+
+
+	@GetMapping("/helloworld")
+	public String hello() {
+		return"Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankPredictorApplication.class, args);
+	}
+
+}
